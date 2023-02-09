c2a224024fe887545914e20836e6d2d154f1d53e
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2023-02-09 15:24:18 +0200

    add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(function () {
-  var mixer = mixitup('.portfolio__content');
-
   $('.slider-blog__inner').slick({
     dots: true,
     prevArrow:
@@ -8,4 +6,10 @@ $(function () {
     nextArrow:
       '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
   });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  const mixer = mixitup('.portfolio__content');
 });
